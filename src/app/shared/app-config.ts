e6bbcca8a3a73d19cd02fef43f2b7f30c2e45b57
sabsowitz <sabsowitz@yahoo.com>
2018-09-05 15:06:51 -0400

    - adding functions to return url for api calls in order to increase reusability

--- src/app/shared/app-config.ts
+++ src/app/shared/app-config.ts
@@ -2,4 +2,21 @@ export class AppConfig {
   public static key: string = '';
   public static cameras: string[] = undefined;
   public static rovers: string[] = undefined;
+
+  url: string;
+
+  returntNasaUrl() {
+    const url = 'https://api.nasa.gov';
+    return url;
+  }
+
+  returnMarsUrl() {
+    const url = 'https://mars-photos.herokuapp.com';
+    return url;
+  }
+
+  returnSpacexUrl() {
+    const url = 'https://api.spacexdata.com';
+    return url;
+  }
 }
